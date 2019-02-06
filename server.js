91f45951a68494fb6c3cad87c3df1db2ef205fd8
toinhao1 <toinhao89@gmail.com>
2019-02-06 19:19:15 -0300

    Added route files

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
 const app = express();
 
 //DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 //Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB Connected"))
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get('/', (req, res) => res.send('Hello World'));
+
+//Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
